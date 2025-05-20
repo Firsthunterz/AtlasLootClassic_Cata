7584fd43751aa1ad08c82c6ac2303868af55fd6a
Firsthunter <101999774+Firsthunterz@users.noreply.github.com>
2025-05-19 21:33:36 -0400

    Added MoP versions

--- AtlasLootClassic_Collections/private.lua
+++ AtlasLootClassic_Collections/private.lua
@@ -6,6 +6,7 @@ private.WORLD_EPICS = {
     [AtlasLoot.BC_VERSION_NUM]          = "WorldEpicsBC",
     [AtlasLoot.WRATH_VERSION_NUM]       = "WorldEpicsWrath",
     [AtlasLoot.CATA_VERSION_NUM]        = "WorldEpicsCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "WorldEpicsMoP",
 }
 
 private.MOUNTS = {
@@ -13,6 +14,7 @@ private.MOUNTS = {
     [AtlasLoot.BC_VERSION_NUM]          = "MountsBC",
     [AtlasLoot.WRATH_VERSION_NUM]       = "MountsWrath",
     [AtlasLoot.CATA_VERSION_NUM]        = "MountsCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "MountsMoP",
 }
 
 private.TABARDS = {
@@ -20,6 +22,8 @@ private.TABARDS = {
     [AtlasLoot.BC_VERSION_NUM]          = "TabardsBC",
     [AtlasLoot.WRATH_VERSION_NUM]       = "TabardsWrath",
     [AtlasLoot.CATA_VERSION_NUM]        = "TabardsCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "TabardsMoP",
+
 }
 
 private.LEGENDARYS = {
@@ -27,6 +31,7 @@ private.LEGENDARYS = {
     [AtlasLoot.BC_VERSION_NUM]          = "LegendarysBC",
     [AtlasLoot.WRATH_VERSION_NUM]       = "LegendarysWrath",
     [AtlasLoot.CATA_VERSION_NUM]        = "LegendarysCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "LegendarysMoP",
 }
 
 private.COMPANIONS = {
@@ -34,11 +39,13 @@ private.COMPANIONS = {
     [AtlasLoot.BC_VERSION_NUM]          = "CompanionsBC",
     [AtlasLoot.WRATH_VERSION_NUM]       = "CompanionsWrath",
     [AtlasLoot.CATA_VERSION_NUM]        = "CompanionsCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "CompanionsMoP",
 }
 
 private.COOKING_VENDOR = {
     [AtlasLoot.WRATH_VERSION_NUM]       = "CookingVendorWrath",
-    [AtlasLoot.CATA_VERSION_NUM]        = "CookingVendorCata"
+    [AtlasLoot.CATA_VERSION_NUM]        = "CookingVendorCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "CookingVendorMoP",
 }
 
 private.SCOURGE_INVASION = {
@@ -49,29 +56,35 @@ private.SCOURGE_INVASION = {
 private.LUNAR_FESTIVAL = {
     [AtlasLoot.CLASSIC_VERSION_NUM]     = "LunarFestival",
     [AtlasLoot.CATA_VERSION_NUM]        = "LunarFestivalCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "LunarFestivalMoP",
 }
 
 private.VALENTINES_DAY = {
     [AtlasLoot.CLASSIC_VERSION_NUM]     = "ValentinesDay",
     [AtlasLoot.WRATH_VERSION_NUM]       = "ValentinesDayWrath",
     [AtlasLoot.CATA_VERSION_NUM]        = "ValentinesDayCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "ValentinesDayMoP",
 }
 
 private.CHILDRENS_WEEK = {
     [AtlasLoot.BC_VERSION_NUM]          = "ChildrensWeekBC",
     [AtlasLoot.WRATH_VERSION_NUM]       = "ChildrensWeekWrath",
+    [AtlasLoot.CATA_VERSION_NUM]        = "ChildrensWeekCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "ChildrensWeekMoP",
 }
 
 private.MIDSUMMER_FESTIVAL = {
     [AtlasLoot.BC_VERSION_NUM]          = "MidsummerFestivalBC",
     [AtlasLoot.WRATH_VERSION_NUM]       = "MidsummerFestivalWrath",
     [AtlasLoot.CATA_VERSION_NUM]        = "MidsummerFestivalCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "MidsummerFestivalMoP",
 }
 
 private.BREWFEST = {
     [AtlasLoot.BC_VERSION_NUM]          = "BrewfestBC",
     [AtlasLoot.WRATH_VERSION_NUM]       = "BrewfestWrath",
     [AtlasLoot.CATA_VERSION_NUM]        = "BrewfestCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "BrewfestMoP",
 }
 
 private.HALLOWEEN = {
@@ -79,13 +92,16 @@ private.HALLOWEEN = {
     [AtlasLoot.BC_VERSION_NUM]          = "HalloweenBC",
     [AtlasLoot.WRATH_VERSION_NUM]       = "HalloweenWrath",
     [AtlasLoot.CATA_VERSION_NUM]        = "HalloweenCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "HalloweenMoP",
 }
 
 private.WINTER_VEIL = {
     [AtlasLoot.CLASSIC_VERSION_NUM]     = "WinterVeil",
     [AtlasLoot.CATA_VERSION_NUM]        = "WinterVeilCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "WinterVeilMoP",
 }
 private.NOBLEGARDEN = {
     [AtlasLoot.CLASSIC_VERSION_NUM]     = "Noblegarden",
     [AtlasLoot.CATA_VERSION_NUM]        = "NoblegardenCata",
+    [AtlasLoot.MOP_VERSION_NUM]        = "NoblegardenMoP",
 }
